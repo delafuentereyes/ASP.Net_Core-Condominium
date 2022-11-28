cd596b02ff4b6e07ee4897616cfe331fef9de99d
jesusdelafuente <jesusdelafureyesjdlfr@gmail.com>
2022-11-28 14:45:37 -0600

    Update Create tables.sql

--- Create tables.sql
+++ Create tables.sql
@@ -51,35 +51,23 @@ GO
 
 CREATE TABLE tblVehiculos(
 	ID_Vehiculo INT IDENTITY(1,1) NOT NULL,
-	Marca_Vehiculo VARCHAR(20) NOT NULL,
+	Marca_Vehiculo VARCHAR(100) NOT NULL,
+	Modelo_Vehiculo VARCHAR(100) NOT NULL,
+	Placa_Vehiculo VARCHAR(20) NOT NULL,
+	Color_Vehiculo VARCHAR(20) NOT NULL,
 
 	CONSTRAINT PKtblVehiculos PRIMARY KEY(ID_Vehiculo)
-)
-GO
-
-CREATE TABLE tblModelos(
-	ID_Modelo Int Identity(1,1) NOT NULL,
-	ID_Vehiculo int NOT NULL, 
-	Modelo_Vehiculo Varchar(100) NOT NULL,
-
-	CONSTRAINT PKtblModelos PRIMARY KEY(ID_MODELO),
-	CONSTRAINT FKtblModelos FOREIGN KEY(ID_Vehiculo) REFERENCES tblVehiculos(ID_Vehiculo)
 )
 GO
 
 CREATE TABLE tblVehiculosxCondominos(
 	ID_Usuario INT NOT NULL,
 	ID_Vehiculo INT NOT NULL,
-	ID_Modelo INT NOT NULL,
 
-	Placa_Vehiculo VARCHAR(20) NOT NULL,
-	Color_Vehiculo VARCHAR(20) NOT NULL,
+	CONSTRAINT PKtblVehiculosxCondominos PRIMARY KEY(ID_Usuario, ID_Vehiculo),
 
-	CONSTRAINT PKtblVehiculosxCondominos PRIMARY KEY(ID_Usuario, ID_Vehiculo, ID_Modelo),
 	CONSTRAINT FKtblVehiculosxCondominos1 FOREIGN KEY(ID_Usuario) REFERENCES tblUsuarios(ID_Usuario),
-	CONSTRAINT FKtblVehiculosxCondominos2 FOREIGN KEY(ID_Vehiculo) REFERENCES tblVehiculos(ID_Vehiculo),
-	CONSTRAINT FKtblVehiculosxCondominos3 FOREIGN KEY(ID_Modelo) REFERENCES tblModelos(ID_Modelo)
-
+	CONSTRAINT FKtblVehiculosxCondominos2 FOREIGN KEY(ID_Vehiculo) REFERENCES tblVehiculos(ID_Vehiculo)
 )
 GO
 
@@ -101,17 +89,12 @@ CREATE TABLE tblVisitas(
 	Color_Vehiculo VARCHAR(10) NOT NULL,
 	
 	ID_Vehiculo INT NOT NULL,
-	ID_Modelo INT NOT NULL,
 
 	CONSTRAINT PKtblVisitas PRIMARY KEY (ID_Visita),
 
 	CONSTRAINT FKtblVisitas1 FOREIGN KEY (ID_Vehiculo)
 	REFERENCES tblVehiculos (ID_Vehiculo),
-
-	CONSTRAINT FKtblVisitas2 FOREIGN KEY (ID_Modelo)
-	REFERENCES tblModelos (ID_Modelo),
-
-	CONSTRAINT FKtblVisitas3 FOREIGN KEY (ID_TipoVisita)
+	CONSTRAINT FKtblVisitas2 FOREIGN KEY (ID_TipoVisita)
 	REFERENCES tblTiposVisitas (ID_TipoVisita),
 )
 GO
