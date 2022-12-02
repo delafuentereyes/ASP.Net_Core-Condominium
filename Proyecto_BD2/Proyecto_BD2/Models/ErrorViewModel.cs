eb25ec0314039c673d829575074811ff61c3abbe
Jesús De la Fuente <jesusdelafureyesjdlfr@gmail.com>
2022-12-02 17:13:13 -0600

    Creación de proyecto

---
+++ Proyecto_BD2/Proyecto_BD2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Proyecto_BD2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
